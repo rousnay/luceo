3381c9cb47aeb2361a000fe18d140d8bc47d37e5
Md. Mozahidur Rahman <mr.rousnay@gmail.com>
2016-08-18 19:09:18 +0600

    fix css bugs

--- page-template/page-blog.php
+++ page-template/page-blog.php
@@ -5,39 +5,18 @@
  * @package Luceo
  */
 get_header(); ?>
-
 <div class="container full-width">
 	<div class="row">
-
 		<div class="col-md-3 sidebar left-sidebar" role="complementary">
 		<?php dynamic_sidebar( 'blog_widgets' ); ?>
 		</div>
-
 		<div class="col-md-9 blog-content">
-
-			<div id="filters" class="select-filter">
-				<select class="filtering">
-			        <option value=".category-1">category-1</option>    
-			        <option value=".category-2">category-2</option>
-			        <option value=".category-3">category-3</option>
-			        <option value=".all">All</option>
-			    </select>    
-			</div>
-
-			<ul id="filters">
-		        <li class="events" data-filter=".category-1"><a href="javascript:void(0)">category-1</a></li>
-		        <li class="workshops" data-filter=".category-2"><a href="javascript:void(0)">category-2</a></li>
-		        <li class="products" data-filter=".category-3"><a href="javascript:void(0)">category-3</a></li>
-		        <li class="active all" data-filter=".all"><a href="javascript:void(0)">All</a></li>
-		    </ul>
-
-
 			<div id="post-listing-isotope" class="row">
 				<?php 
 							$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 			$args = array(
 				'post_type' => 'post',
-				'posts_per_page' => 5,
+				'posts_per_page' => 4,
 				'paged' => $paged
 				);
 				$loop = new WP_Query( $args );
@@ -57,7 +36,6 @@ get_header(); ?>
 				else :  
 					$tax = '';          
 				endif; ?>
-
 				<div class="all post-item col-xs-12 col-sm-6 <?php echo $tax; ?>">
 						<?php
 							$post_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'luceo_blog_listing');
@@ -65,19 +43,12 @@ get_header(); ?>
 							$post_url	= get_permalink();
 							$content 	= get_the_content();
 						?>
-					
-
-
-
 						<div class="thumbnail thumbnail-hover">
 							<div class="blog-img">
 								<img class="img-responsive" src="<?php echo $thumb_url; ?>" >
 							</div>
 							<a href="<?php echo $post_url ?>" title="<?php  the_title_attribute() ?>" class="overlay"></a>
 						</div>
-
-
-
 						<div class="entry">
 						<h3><a href="<?php echo $post_url ?>"> <?php the_title() ?> </a></h3>
 						<div class="entry-content"><?php echo wp_trim_words( $content , '27' ) ?></div>
@@ -86,21 +57,16 @@ get_header(); ?>
 						</div>
 						</div>
 				</div>
-
 			<?php endwhile; ?>
 		</div>
-
-						<div class="row">
+		<div class="row">
 		<div class="col-xs-12">
-		<?php if (function_exists("pagination")) {
-		    pagination($loop->max_num_pages);
-		} ?>
+			<?php if (function_exists("pagination")) {
+			    pagination($loop->max_num_pages);
+			} ?>
+			</div>
 		</div>
-	</div>
-
 </div>
-
 </div><!-- .row -->
 </div><!-- .container -->
-
 <?php get_footer(); ?>
